803c08abc8ecfc5ef3f9244b320455b999c65c3f
Elviss Strazdins <elviss@elviss.lv>
2016-06-08 02:37:49 +0300

    Detect input device types

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include <stdio.h>
 #include <bcm_host.h>
 
@@ -8,9 +10,21 @@
 #include <fcntl.h>
 #include <linux/input.h>
 
+#include <glob.h>
+
 EGLDisplay display;
 EGLSurface surface;
 EGLContext context;
+
+#define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
+#define BITS_PER_LONG (8 * sizeof(long))
+#define BITS_TO_LONGS(nr) DIV_ROUND_UP(nr, BITS_PER_LONG)
+
+static inline int
+bit_is_set(const unsigned long* array, int bit)
+{
+    return !!(array[bit / BITS_PER_LONG] & (1LL << (bit % BITS_PER_LONG)));
+}
 
 int main(int argc, char* argv[])
 {
@@ -18,7 +32,7 @@ int main(int argc, char* argv[])
 
     // get an EGL display connection
     display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    
+
     if (!display)
     {
         printf("Failed to get display\n");
@@ -27,7 +41,7 @@ int main(int argc, char* argv[])
 
     // initialize the EGL display connection
     //EGLBoolean result;
-    
+
     if (!eglInitialize(display, NULL, NULL))
     {
         printf("Failed to initialize EGL\n");
@@ -46,14 +60,14 @@ int main(int argc, char* argv[])
     };
     EGLConfig config;
     EGLint numConfig;
-    
+
     if (!eglChooseConfig(display, attributeList, &config, 1, &numConfig))
     {
         printf("Failed to choose EGL config\n");
         return 1;
     }
 
-    // get an appropriate EGL frame buffer configuration    
+    // get an appropriate EGL frame buffer configuration
     if (!eglBindAPI(EGL_OPENGL_ES_API))
     {
         printf("Failed to bind OpenGL ES API\n");
@@ -67,7 +81,7 @@ int main(int argc, char* argv[])
         EGL_NONE
     };
     context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttributes);
-    
+
     if (context == EGL_NO_CONTEXT)
     {
         printf("Failed to create EGL context\n");
@@ -78,7 +92,7 @@ int main(int argc, char* argv[])
     uint32_t screenWidth;
     uint32_t screenHeight;
     int32_t success = graphics_get_display_size(0, &screenWidth, &screenHeight);
-    
+
     if (success == -1)
     {
         printf("Failed to get display size\n");
@@ -90,21 +104,19 @@ int main(int argc, char* argv[])
     dstRect.y = 0;
     dstRect.width = screenWidth;
     dstRect.height = screenHeight;
-    
+
     VC_RECT_T srcRect;
     srcRect.x = 0;
     srcRect.y = 0;
     srcRect.width = screenWidth;
     srcRect.height = screenHeight;
-    /*srcRect.width = screen_width << 16;
-    srcRect.height = screen_height << 16;*/
 
     DISPMANX_DISPLAY_HANDLE_T dispmanDisplay = vc_dispmanx_display_open(0);
     DISPMANX_UPDATE_HANDLE_T dispmanUpdate = vc_dispmanx_update_start(0);
 
     DISPMANX_ELEMENT_HANDLE_T dispmanElement = vc_dispmanx_element_add(dispmanUpdate, dispmanDisplay,
                 0, &dstRect, 0,
-                &srcRect, DISPMANX_PROTECTION_NONE, 
+                &srcRect, DISPMANX_PROTECTION_NONE,
                 0, 0, DISPMANX_NO_ROTATE);
 
     static EGL_DISPMANX_WINDOW_T nativewindow;
@@ -126,71 +138,169 @@ int main(int argc, char* argv[])
         printf("Failed to set current EGL context\n");
         return 1;
     }
-    
+
     // input
+    struct InputDeviceRPI
+    {
+        enum DeviceClass
+        {
+            CLASS_KEYBOARD = 1,
+            CLASS_MOUSE = 2,
+            CLASS_TOUCHPAD = 4,
+            CLASS_GAMEPAD = 8
+        };
+
+        uint32_t deviceClass = 0;
+        int fd = 0;
+    };
+
+    int maxFd = 0;
+    std::vector<InputDeviceRPI> inputDevices;
+    uint32_t mouseX = 0;
+    uint32_t mouseY = 0;
     char TEMP[256];
-    
-    int keyboardFd = open("/dev/input/event0", O_RDONLY);
-    
-    if (keyboardFd == -1)
+
+    glob_t g;
+    int result = glob("/dev/input/event*", GLOB_NOSORT, NULL, &g);
+
+    if (result == GLOB_NOMATCH)
     {
-        printf("No keyboard installed\n");
+        printf("No event devices found\n");
         return 1;
     }
-    
-    if (ioctl(keyboardFd, EVIOCGRAB, (void *)1) < 0)
+    else if (result)
     {
-        printf("Failed to get grab device\n");
+        printf("Could not read /dev/input/event*\n");
         return 1;
     }
-    
-    memset(TEMP, 0, sizeof(TEMP));
-    if (ioctl(keyboardFd, EVIOCGNAME(sizeof(TEMP) - 1), TEMP) < 0)
+
+    for (size_t i = 0; i < g.gl_pathc; i++)
     {
-        printf("Failed to get device name\n");
-        return 1;
+        InputDeviceRPI inputDevice;
+
+        inputDevice.fd = open(g.gl_pathv[i], O_RDONLY);
+        if (inputDevice.fd == -1)
+        {
+            printf("Failed to open device file descriptor\n");
+            continue;
+        }
+
+        if (ioctl(inputDevice.fd, EVIOCGRAB, (void *)1) == -1)
+        {
+            printf("Failed to get grab device\n");
+        }
+
+        memset(TEMP, 0, sizeof(TEMP));
+        if (ioctl(inputDevice.fd, EVIOCGNAME(sizeof(TEMP) - 1), TEMP) == -1)
+        {
+            printf("Failed to get device name\n");
+        }
+        else
+        {
+            printf("Got device: %s\n", TEMP);
+        }
+
+        unsigned long eventBits[BITS_TO_LONGS(EV_CNT)];
+        unsigned long absBits[BITS_TO_LONGS(ABS_CNT)];
+        unsigned long relBits[BITS_TO_LONGS(REL_CNT)];
+        unsigned long keyBits[BITS_TO_LONGS(KEY_CNT)];
+
+        if (ioctl(inputDevice.fd, EVIOCGBIT(0, sizeof(eventBits)), eventBits) == -1 ||
+            ioctl(inputDevice.fd, EVIOCGBIT(EV_ABS, sizeof(absBits)), absBits) == -1 ||
+            ioctl(inputDevice.fd, EVIOCGBIT(EV_REL, sizeof(relBits)), relBits) == -1 ||
+            ioctl(inputDevice.fd, EVIOCGBIT(EV_KEY, sizeof(keyBits)), keyBits) == -1)
+        {
+            printf("Failed to get device event bits\n");
+        }
+        
+        if (bit_is_set(eventBits, EV_KEY) && (
+             bit_is_set(keyBits, KEY_1) ||
+             bit_is_set(keyBits, KEY_2) ||
+             bit_is_set(keyBits, KEY_3) ||
+             bit_is_set(keyBits, KEY_4) ||
+             bit_is_set(keyBits, KEY_5) ||
+             bit_is_set(keyBits, KEY_6) ||
+             bit_is_set(keyBits, KEY_7) ||
+             bit_is_set(keyBits, KEY_8) ||
+             bit_is_set(keyBits, KEY_9) ||
+             bit_is_set(keyBits, KEY_0)
+            ))
+        {
+            printf("Keyboard\n");
+            inputDevice.deviceClass = InputDeviceRPI::CLASS_KEYBOARD;
+        }
+        
+        if (bit_is_set(eventBits, EV_ABS) && bit_is_set(absBits, ABS_X) && bit_is_set(absBits, ABS_Y))
+        {
+            if (bit_is_set(keyBits, BTN_STYLUS) || bit_is_set(keyBits, BTN_TOOL_PEN))
+            {
+                printf("Tablet\n");
+                inputDevice.deviceClass |= InputDeviceRPI::CLASS_TOUCHPAD;
+            } 
+            else if (bit_is_set(keyBits, BTN_TOOL_FINGER) && !bit_is_set(keyBits, BTN_TOOL_PEN))
+            {
+                printf("Touchpad\n");
+                inputDevice.deviceClass |= InputDeviceRPI::CLASS_TOUCHPAD;
+            }
+            else if (bit_is_set(keyBits, BTN_MOUSE))
+            {
+                printf("Mouse\n");
+                inputDevice.deviceClass |= InputDeviceRPI::CLASS_MOUSE;
+            }
+            else if (bit_is_set(keyBits, BTN_TOUCH))
+            {
+                printf("Touchscreen\n");
+                inputDevice.deviceClass |= InputDeviceRPI::CLASS_TOUCHPAD;
+            }
+        }
+        else if (bit_is_set(eventBits, EV_REL) && bit_is_set(relBits, REL_X) && bit_is_set(relBits, REL_Y))
+        {
+            if (bit_is_set(keyBits, BTN_MOUSE))
+            {
+                printf("Mouse\n");
+                inputDevice.deviceClass |= InputDeviceRPI::CLASS_MOUSE;
+            }
+        }
+
+        if (bit_is_set(keyBits, BTN_JOYSTICK))
+        {
+            printf("Joystick\n");
+            inputDevice.deviceClass = InputDeviceRPI::CLASS_GAMEPAD;
+        }
+
+        if (bit_is_set(keyBits, BTN_GAMEPAD))
+        {
+            printf("Gamepad\n");
+            inputDevice.deviceClass = InputDeviceRPI::CLASS_GAMEPAD;
+        }
+
+        if (inputDevice.fd > maxFd)
+        {
+            maxFd = inputDevice.fd;
+        }
+
+        inputDevices.push_back(inputDevice);
     }
-    
-    printf("Device name: %s\n", TEMP);
-    
-    int mouseFd = open("/dev/input/event1", O_RDONLY);
-    
-    if (mouseFd == -1)
-    {
-        printf("No mouse installed\n");
-        return 1;
-    }
-    
-    if (ioctl(mouseFd, EVIOCGRAB, (void *)1) < 0)
-    {
-        printf("Failed to get grab device\n");
-        return 1;
-    }
-    
-    memset(TEMP, 0, sizeof(TEMP));
-    if (ioctl(mouseFd, EVIOCGNAME(sizeof(TEMP) - 1), TEMP) < 0)
-    {
-        printf("Failed to get device name\n");
-        return 1;
-    }
-    printf("Device name: %s\n", TEMP);
-    
+
+    globfree(&g);
+
     fd_set rfds;
     struct timeval tv;
-    int maxFd = keyboardFd;
-    if (mouseFd > maxFd) maxFd = mouseFd;
-    
+
     for(;;)
     {
         FD_ZERO(&rfds);
-        FD_SET(keyboardFd, &rfds);
-        FD_SET(mouseFd, &rfds);
-        
+
+        for (const InputDeviceRPI& inputDevice : inputDevices)
+        {
+            FD_SET(inputDevice.fd, &rfds);
+        }
+
         tv.tv_sec = 0;
         tv.tv_usec = 0;
-        
+
         int retval = select(maxFd + 1, &rfds, NULL, NULL, &tv);
-        
+
         if (retval == -1)
         {
             printf("Select failed\n");
@@ -198,116 +308,113 @@ int main(int argc, char* argv[])
         }
         else if (retval > 0)
         {
-            if (FD_ISSET(keyboardFd, &rfds))
+            for (const InputDeviceRPI& inputDevice : inputDevices)
             {
-                ssize_t bytesRead = read(keyboardFd, TEMP, sizeof(TEMP));
-                
-                printf("Got input from keyboard, read %d bytes\n", bytesRead);
-                
-                struct input_event event;
-                
-                for (int i = 0; i < bytesRead - (int)sizeof(event) + 1; i += sizeof(event))
+                if (FD_ISSET(inputDevice.fd, &rfds))
                 {
-                    memcpy(&event, TEMP +  i, sizeof(event));
-                    
-                    printf("Timestamp: %d.%d, type: %d", (uint32_t)event.time.tv_sec, (uint32_t)event.time.tv_usec, event.type);
-                    
-                    switch (event.type)
+                    ssize_t bytesRead = read(inputDevice.fd, TEMP, sizeof(TEMP));
+
+                    if (bytesRead == -1)
                     {
-                    case EV_SYN:
-                        printf(", EV_SYN");
-                        break;
-                    case EV_KEY:
-                        printf(", EV_KEY");
-                        break;
-                    case EV_MSC:
-                        printf(", EV_MSC");
-                        break;
-                    case EV_REP:
-                        printf(", EV_REP");
-                        break;
+                        printf("Failed to read input");
                     }
-                    
-                    printf(", value: %d, key: %d\n", event.value, event.code);
-                }
-            }
-            else if (FD_ISSET(mouseFd, &rfds))
-            {
-                ssize_t bytesRead = read(mouseFd, TEMP, sizeof(TEMP));
-                
-                printf("Got input from mouse, read %d bytes\n", bytesRead);
-                
-                struct input_event event;
-                
-                for (int i = 0; i < bytesRead - (int)sizeof(event) + 1; i += sizeof(event))
-                {
-                    memcpy(&event, TEMP +  i, sizeof(event));
-                    
-                    printf("Timestamp: %d.%d, type: %d", (uint32_t)event.time.tv_sec, (uint32_t)event.time.tv_usec, event.type);
-                    
-                    switch (event.type)
+
+                    printf("Got input, read %d bytes\n", bytesRead);
+
+                    for (ssize_t i = 0; i < bytesRead - static_cast<ssize_t>(sizeof(input_event)) + 1; i += sizeof(input_event))
                     {
-                    case EV_SYN:
-                        printf(", EV_SYN");
-                        break;
-                    case EV_KEY:
-                        printf(", EV_KEY");
-                        break;
-                    case EV_MSC:
-                        printf(", EV_MSC");
-                        break;
-                    case EV_REL:
-                        printf(", EV_REL");
-                        break;
+                        input_event* event = reinterpret_cast<input_event*>(TEMP + i);
+
+                        if (inputDevice.deviceClass & InputDeviceRPI::CLASS_KEYBOARD)
+                        {
+                            printf("Timestamp: %d.%d, type: %d", (uint32_t)event->time.tv_sec, (uint32_t)event->time.tv_usec, event->type);
+
+                            switch (event->type)
+                            {
+                            case EV_SYN:
+                                printf(", EV_SYN");
+                                break;
+                            case EV_KEY:
+                                printf(", EV_KEY");
+                                break;
+                            case EV_MSC:
+                                printf(", EV_MSC");
+                                break;
+                            case EV_REP:
+                                printf(", EV_REP");
+                                break;
+                            }
+
+                            printf(", value: %d, key: %d\n", event->value, event->code);
+
+                            if (event->type == EV_KEY && event->code == KEY_ESC)
+                            {
+                                return 0;
+                            }
+                        }
+
+                        if (inputDevice.deviceClass & InputDeviceRPI::CLASS_MOUSE)
+                        {
+                            printf("Timestamp: %d.%d, type: %d", (uint32_t)event->time.tv_sec, (uint32_t)event->time.tv_usec, event->type);
+
+                            switch (event->type)
+                            {
+                            case EV_SYN:
+                                printf(", EV_SYN");
+                                break;
+                            case EV_KEY:
+                                printf(", EV_KEY");
+                                break;
+                            case EV_MSC:
+                                printf(", EV_MSC");
+                                break;
+                            case EV_REL:
+                                printf(", EV_REL");
+                                break;
+                            }
+
+                            printf(", value: %d, key: %d\n", event->value, event->code);
+                        }
                     }
-                    
-                    printf(", value: %d, key: %d\n", event.value, event.code);
                 }
             }
         }
-    
+
         glClearColor(1.0, 0.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
         glFlush();
 
         eglSwapBuffers(display, surface);
     }
-    
-    if (ioctl(mouseFd, EVIOCGRAB, (void *)0) < 0)
+
+    for (const InputDeviceRPI& inputDevice : inputDevices)
     {
-        printf("Failed to get grab device\n");
-    }
-    
-    if (close(mouseFd) == -1)
-    {
-        printf("Failed to close mouse file descriptor\n");
-    }
-    
-    if (ioctl(keyboardFd, EVIOCGRAB, (void *)0) < 0)
-    {
-        printf("Failed to get grab device\n");
+        if (ioctl(inputDevice.fd, EVIOCGRAB, (void*)0) == -1)
+        {
+            printf("Failed to release device\n");
+        }
+
+        if (close(inputDevice.fd) == -1)
+        {
+            printf("Failed to close file descriptor\n");
+        }
     }
 
-    if (close(keyboardFd) == -1)
-    {
-        printf("Failed to close keyboard file descriptor\n");
-    }
-    
     if (!eglDestroySurface(display, surface))
     {
         printf("Failed to destroy EGL surface\n");
     }
-    
+
     if (!eglDestroyContext(display, context))
     {
         printf("Failed to destroy EGL context\n");
     }
-    
+
     if (!eglTerminate(display))
     {
         printf("Failed to terminate EGL\n");
     }
-    
+
     bcm_host_deinit();
 
     return 0;
